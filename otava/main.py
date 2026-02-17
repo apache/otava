nyrkio/hunter

Make regressions subcommand support --output param
It's useful to get the output of "hunter regressions" in JSON format for
post-processing. The default behaviour if the user doesn't pass
--output should be the same as before this change.

hunter/main.py
@@ -16,7 +16,7 @@ from hunter.data_selector import DataSelector
 from hunter.grafana import Annotation, Grafana, GrafanaError
 from hunter.graphite import GraphiteError
 from hunter.importer import DataImportError, Importers
-from hunter.report import Report, ReportType
+from hunter.report import ChangePointReport, ReportType, RegressionsReport
 from hunter.series import AnalysisOptions, AnalyzedSeries, compare
 from hunter.slack import NotificationError, SlackNotifier
 from hunter.test_config import GraphiteTestConfig, TestConfig, TestConfigError
@@ -94,7 +94,7 @@ class Hunter:
         series = importer.fetch_data(test, selector)
         analyzed_series = series.analyze(options)
         change_points = analyzed_series.change_points_by_time
-        report = Report(series, change_points)
+        report = ChangePointReport(series, change_points)
         produced_report = report.produce_report(test.name, report_type)
         print(produced_report)
         return analyzed_series
@@ -208,6 +208,7 @@ class Hunter:
         test: TestConfig,
         selector: DataSelector,
         options: AnalysisOptions,
+        report_type: ReportType,
         ignore_direction: bool = False,
     ) -> bool:
         importer = self.__importers.get(test)
@@ -256,25 +257,17 @@ class Hunter:
             direction = baseline_series.metric(metric_name).direction
             m1 = stats.mean_1
             m2 = stats.mean_2
-            change_percent = stats.forward_rel_change() * 100.0
             if ignore_direction:
                 mean_diff = m2 != m1
             else:
                 mean_diff = m2 * direction < m1 * direction
 
             if mean_diff and stats.pvalue < options.max_pvalue:
-                regressions.append(
-                    "    {:16}: {:#8.3g} --> {:#8.3g} ({:+6.1f}%)".format(
-                        metric_name, m1, m2, change_percent
-                    )
-                )
+                regressions.append((metric_name, stats))
 
-        if regressions:
-            print(f"{test.name}:")
-            for r in regressions:
-                print(r)
-        else:
-            print(f"{test.name}: OK")
+        report = RegressionsReport(regressions)
+        produced_report = report.produce_report(test.name, report_type)
+        print(produced_report)
         return len(regressions) > 0
 
     def __maybe_create_slack_notifier(self):
@@ -455,6 +448,14 @@ def setup_analysis_options_parser(parser: argparse.ArgumentParser):
         help="use the original edivisive algorithm with no windowing "
         "and weak change points analysis improvements",
     )
+    parser.add_argument(
+        "--output",
+        help="Output format for the generated report.",
+        choices=list(ReportType),
+        dest="report_type",
+        default=ReportType.LOG,
+        type=ReportType,
+    )
 
 
 def analysis_options_from_args(args: argparse.Namespace) -> AnalysisOptions:
@@ -517,14 +518,6 @@ def script_main(conf: Config, args: List[str] = None):
         help="Sets a limit on the date range of the Change Point History reported to Slack. Same syntax as --since.",
         metavar="DATE",
         dest="cph_report_since",
-    )
-    analyze_parser.add_argument(
-        "--output",
-        help="Output format for the generated report.",
-        choices=list(ReportType),
-        dest="report_type",
-        default=ReportType.LOG,
-        type=ReportType,
     )
     setup_data_selector_parser(analyze_parser)
     setup_analysis_options_parser(analyze_parser)
@@ -616,6 +609,7 @@ def script_main(conf: Config, args: List[str] = None):
                         selector=data_selector,
                         options=options,
                         ignore_direction=args.ignore_direction,
+                        report_type=args.report_type,
                     )
                     if regressions:
                         regressing_test_count += 1
@@ -625,12 +619,15 @@ def script_main(conf: Config, args: List[str] = None):
                 except DataImportError as err:
                     logging.error(err.message)
                     errors += 1
-            if regressing_test_count == 0:
-                print("No regressions found!")
-            elif regressing_test_count == 1:
-                print("Regressions in 1 test found")
-            else:
-                print(f"Regressions in {regressing_test_count} tests found")
+
+            if args.report_type == ReportType.LOG:
+                if regressing_test_count == 0:
+                    print("No regressions found!")
+                elif regressing_test_count == 1:
+                    print("Regressions in 1 test found")
+                else:
+                    print(f"Regressions in {regressing_test_count} tests found")
+
             if errors > 0:
                 print("Some tests were skipped due to import / analyze errors. Consult error log.")
 

hunter/report.py
@@ -1,9 +1,10 @@
-from collections import OrderedDict
+from collections import OrderedDict, defaultdict
 from enum import Enum, unique
-from typing import List
+from typing import List, Tuple
 
 from tabulate import tabulate
 
+from hunter.analysis import ComparativeStats
 from hunter.series import ChangePointGroup, Series
 from hunter.util import format_timestamp, insert_multiple, remove_common_prefix
 
@@ -17,7 +18,7 @@ class ReportType(Enum):
         return self.value
 
 
-class Report:
+class ChangePointReport:
     __series: Series
     __change_points: List[ChangePointGroup]
 
@@ -83,3 +84,46 @@ class Report:
         import json
 
         return json.dumps({test_name: [cpg.to_json() for cpg in self.__change_points]})
+
+
+class RegressionsReport:
+    def __init__(self, regressions: List[Tuple[str, ComparativeStats]]):
+        self.__regressions = regressions
+
+    def produce_report(self, test_name: str, report_type: ReportType):
+        if report_type == ReportType.LOG:
+            return self.__format_log(test_name)
+        elif report_type == ReportType.JSON:
+            return self.__format_json(test_name)
+        else:
+            from hunter.main import HunterError
+
+            raise HunterError(f"Unknown report type: {report_type}")
+
+    def __format_log(self, test_name: str) -> str:
+        if not self.__regressions:
+            return f"{test_name}: OK"
+
+        output = f"{test_name}:"
+        for metric_name, stats in self.__regressions:
+            m1 = stats.mean_1
+            m2 = stats.mean_2
+            change_percent = stats.forward_change_percent()
+            output += "\n    {:16}: {:#8.3g} --> {:#8.3g} ({:+6.1f}%)".format(
+                metric_name, m1, m2, change_percent
+            )
+        return output
+
+    def __format_json(self, test_name: str) -> str:
+        import json
+
+        if not self.__regressions:
+            return json.dumps({test_name: []})
+
+        output = defaultdict(list)
+        for metric_name, stats in self.__regressions:
+            obj = {"metric": metric_name}
+            obj.update(stats.to_json())
+            output[test_name].append(obj)
+
+        return json.dumps(output)

apache/otava

(cherry picked from commit 3400a3cfd3cb5ac044ad697a9c95afbeeda058e8)

otava/main.py
@@ -35,7 +35,7 @@ from otava.grafana import Annotation, Grafana, GrafanaError
 from otava.graphite import GraphiteError
 from otava.importer import DataImportError, Importers
 from otava.postgres import Postgres, PostgresError
-from otava.report import Report, ReportType
+from otava.report import ChangePointReport, RegressionsReport, ReportType
 from otava.series import AnalysisOptions, AnalyzedSeries, compare
 from otava.slack import NotificationError, SlackNotifier
 from otava.test_config import (
@@ -123,7 +123,7 @@ class Otava:
         series = importer.fetch_data(test, selector)
         analyzed_series = series.analyze(options)
         change_points = analyzed_series.change_points_by_time
-        report = Report(series, change_points)
+        report = ChangePointReport(series, change_points)
         produced_report = report.produce_report(test.name, report_type)
         print(produced_report)
         return analyzed_series
@@ -261,6 +261,7 @@ class Otava:
         test: TestConfig,
         selector: DataSelector,
         options: AnalysisOptions,
+        report_type: ReportType,
         ignore_direction: bool = False,
     ) -> bool:
         importer = self.__importers.get(test)
@@ -309,25 +310,17 @@ class Otava:
             direction = baseline_series.metric(metric_name).direction
             m1 = stats.mean_1
             m2 = stats.mean_2
-            change_percent = stats.forward_rel_change() * 100.0
             if ignore_direction:
                 mean_diff = m2 != m1
             else:
                 mean_diff = m2 * direction < m1 * direction
 
             if mean_diff and stats.pvalue < options.max_pvalue:
-                regressions.append(
-                    "    {:16}: {:#8.3g} --> {:#8.3g} ({:+6.1f}%)".format(
-                        metric_name, m1, m2, change_percent
-                    )
-                )
+                regressions.append((metric_name, stats))
 
-        if regressions:
-            print(f"{test.name}:")
-            for r in regressions:
-                print(r)
-        else:
-            print(f"{test.name}: OK")
+        report = RegressionsReport(regressions)
+        produced_report = report.produce_report(test.name, report_type)
+        print(produced_report)
         return len(regressions) > 0
 
     def __maybe_create_slack_notifier(self):
@@ -508,6 +501,14 @@ def setup_analysis_options_parser(parser: argparse.ArgumentParser):
         help="use the original edivisive algorithm with no windowing "
         "and weak change points analysis improvements",
     )
+    parser.add_argument(
+        "--output",
+        help="Output format for the generated report.",
+        choices=list(ReportType),
+        dest="report_type",
+        default=ReportType.LOG,
+        type=ReportType,
+    )
 
 
 def analysis_options_from_args(args: argparse.Namespace) -> AnalysisOptions:
@@ -584,14 +585,6 @@ def create_otava_cli_parser() -> argparse.ArgumentParser:
         help="Sets a limit on the date range of the Change Point History reported to Slack. Same syntax as --since.",
         metavar="DATE",
         dest="cph_report_since",
-    )
-    analyze_parser.add_argument(
-        "--output",
-        help="Output format for the generated report.",
-        choices=list(ReportType),
-        dest="report_type",
-        default=ReportType.LOG,
-        type=ReportType,
     )
     setup_data_selector_parser(analyze_parser)
     setup_analysis_options_parser(analyze_parser)
@@ -715,6 +708,7 @@ def script_main(conf: Config = None, args: List[str] = None):
                         selector=data_selector,
                         options=options,
                         ignore_direction=args.ignore_direction,
+                        report_type=args.report_type,
                     )
                     if regressions:
                         regressing_test_count += 1
@@ -724,12 +718,15 @@ def script_main(conf: Config = None, args: List[str] = None):
                 except DataImportError as err:
                     logging.error(err.message)
                     errors += 1
-            if regressing_test_count == 0:
-                print("No regressions found!")
-            elif regressing_test_count == 1:
-                print("Regressions in 1 test found")
-            else:
-                print(f"Regressions in {regressing_test_count} tests found")
+
+            if args.report_type == ReportType.LOG:
+                if regressing_test_count == 0:
+                    print("No regressions found!")
+                elif regressing_test_count == 1:
+                    print("Regressions in 1 test found")
+                else:
+                    print(f"Regressions in {regressing_test_count} tests found")
+
             if errors > 0:
                 print("Some tests were skipped due to import / analyze errors. Consult error log.")
 

otava/report.py
@@ -15,12 +15,13 @@
 # specific language governing permissions and limitations
 # under the License.
 
-from collections import OrderedDict
+from collections import OrderedDict, defaultdict
 from enum import Enum, unique
-from typing import List
+from typing import List, Tuple
 
 from tabulate import tabulate
 
+from otava.analysis import TTestStats
 from otava.series import ChangePointGroup, Series
 from otava.util import format_timestamp, insert_multiple, remove_common_prefix
 
@@ -35,7 +36,7 @@ class ReportType(Enum):
         return self.value
 
 
-class Report:
+class ChangePointReport:
     __series: Series
     __change_points: List[ChangePointGroup]
 
@@ -133,3 +134,46 @@ class Report:
             return f"Regressions in {test_name}:" + "\n" + "\n".join(output)
         else:
             return f"No regressions found in {test_name}."
+
+
+class RegressionsReport:
+    def __init__(self, regressions: List[Tuple[str, TTestStats]]):
+        self.__regressions = regressions
+
+    def produce_report(self, test_name: str, report_type: ReportType):
+        if report_type == ReportType.LOG:
+            return self.__format_log(test_name)
+        elif report_type == ReportType.JSON:
+            return self.__format_json(test_name)
+        else:
+            from hunter.main import HunterError
+
+            raise HunterError(f"Unknown report type: {report_type}")
+
+    def __format_log(self, test_name: str) -> str:
+        if not self.__regressions:
+            return f"{test_name}: OK"
+
+        output = f"{test_name}:"
+        for metric_name, stats in self.__regressions:
+            m1 = stats.mean_1
+            m2 = stats.mean_2
+            change_percent = stats.forward_change_percent()
+            output += "\n    {:16}: {:#8.3g} --> {:#8.3g} ({:+6.1f}%)".format(
+                metric_name, m1, m2, change_percent
+            )
+        return output
+
+    def __format_json(self, test_name: str) -> str:
+        import json
+
+        if not self.__regressions:
+            return json.dumps({test_name: []})
+
+        output = defaultdict(list)
+        for metric_name, stats in self.__regressions:
+            obj = {"metric": metric_name}
+            obj.update(stats.to_json())
+            output[test_name].append(obj)
+
+        return json.dumps(output)