nyrkio/hunter

Add --ignore-direction to regressions subcommand
Sometimes users want to know about *any* performance change between a PR
and a baseline branch, i.e. not just whether there's a regression but
also if there's a performance improvement.

hunter/main.py
@@ -204,7 +204,11 @@ class Hunter:
         grafana.delete_annotations(*(a.id for a in annotations))
 
     def regressions(
-        self, test: TestConfig, selector: DataSelector, options: AnalysisOptions
+        self,
+        test: TestConfig,
+        selector: DataSelector,
+        options: AnalysisOptions,
+        ignore_direction: bool = False,
     ) -> bool:
         importer = self.__importers.get(test)
 
@@ -253,7 +257,12 @@ class Hunter:
             m1 = stats.mean_1
             m2 = stats.mean_2
             change_percent = stats.forward_rel_change() * 100.0
-            if m2 * direction < m1 * direction and stats.pvalue < options.max_pvalue:
+            if ignore_direction:
+                mean_diff = m2 != m1
+            else:
+                mean_diff = m2 * direction < m1 * direction
+
+            if mean_diff and stats.pvalue < options.max_pvalue:
                 regressions.append(
                     "    {:16}: {:#8.3g} --> {:#8.3g} ({:+6.1f}%)".format(
                         metric_name, m1, m2, change_percent
@@ -524,6 +533,12 @@ def script_main(conf: Config, args: List[str] = None):
     regressions_parser.add_argument(
         "tests", help="name of the test or group of the tests", nargs="+"
     )
+    regressions_parser.add_argument(
+        "--ignore-direction",
+        help="ignore the direction of the change in performance",
+        dest="ignore_direction",
+        action="store_true",
+    )
     setup_data_selector_parser(regressions_parser)
     setup_analysis_options_parser(regressions_parser)
 
@@ -596,7 +611,12 @@ def script_main(conf: Config, args: List[str] = None):
             errors = 0
             for test in tests:
                 try:
-                    regressions = hunter.regressions(test, selector=data_selector, options=options)
+                    regressions = hunter.regressions(
+                        test,
+                        selector=data_selector,
+                        options=options,
+                        ignore_direction=args.ignore_direction,
+                    )
                     if regressions:
                         regressing_test_count += 1
                 except HunterError as err:

apache/otava

(cherry picked from commit 116eb5a54572846adca1861e1369930330311d2c)

otava/main.py
@@ -257,7 +257,11 @@ class Otava:
                 bigquery.insert_change_point(test, metric_name, attributes, cp)
 
     def regressions(
-        self, test: TestConfig, selector: DataSelector, options: AnalysisOptions
+        self,
+        test: TestConfig,
+        selector: DataSelector,
+        options: AnalysisOptions,
+        ignore_direction: bool = False,
     ) -> bool:
         importer = self.__importers.get(test)
 
@@ -306,7 +310,12 @@ class Otava:
             m1 = stats.mean_1
             m2 = stats.mean_2
             change_percent = stats.forward_rel_change() * 100.0
-            if m2 * direction < m1 * direction and stats.pvalue < options.max_pvalue:
+            if ignore_direction:
+                mean_diff = m2 != m1
+            else:
+                mean_diff = m2 * direction < m1 * direction
+
+            if mean_diff and stats.pvalue < options.max_pvalue:
                 regressions.append(
                     "    {:16}: {:#8.3g} --> {:#8.3g} ({:+6.1f}%)".format(
                         metric_name, m1, m2, change_percent
@@ -594,6 +603,12 @@ def create_otava_cli_parser() -> argparse.ArgumentParser:
     regressions_parser.add_argument(
         "tests", help="name of the test or group of the tests", nargs="+"
     )
+    regressions_parser.add_argument(
+        "--ignore-direction",
+        help="ignore the direction of the change in performance",
+        dest="ignore_direction",
+        action="store_true",
+    )
     config.add_service_option_groups(regressions_parser)
     setup_data_selector_parser(regressions_parser)
     setup_analysis_options_parser(regressions_parser)
@@ -695,7 +710,12 @@ def script_main(conf: Config = None, args: List[str] = None):
             errors = 0
             for test in tests:
                 try:
-                    regressions = otava.regressions(test, selector=data_selector, options=options)
+                    regressions = otava.regressions(
+                        test,
+                        selector=data_selector,
+                        options=options,
+                        ignore_direction=args.ignore_direction,
+                    )
                     if regressions:
                         regressing_test_count += 1
                 except OtavaError as err: